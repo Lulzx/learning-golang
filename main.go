8bac0600c6abc0e93d85119b01b66bce1a3d7005
Lulzx <wiserishabh@gmail.com>
2017-09-28 11:28:37 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,5 +10,19 @@ func main() {
                 log.Fatal(err)
         }
         bot.Handle("/answer", "42")
+        bot.HandleFunc("/timer {seconds}", timerHandler)
         bot.ListenAndServe()
+}
+func timerHandler(m *tbot.Message) {
+        // m.Vars contains all variables, parsed during routing
+        secondsStr := m.Vars["seconds"]
+        // Convert string variable to integer seconds value
+        seconds, err := strconv.Atoi(secondsStr)
+        if err != nil {
+                m.Reply("Invalid number of seconds")
+                return
+        }
+        m.Replyf("Timer for %d seconds started", seconds)
+        time.Sleep(time.Duration(seconds) * time.Second)
+        m.Reply("Time out!")
 }
